66fd6d9c25b5961fa2763da184322f1e6cdf3234
V.Darmakheev <v.darmakheev@playa.ru>
2018-04-20 18:01:52 +0800

    Сделал шаблон проекта на spring boot, подправил yaml

---
+++ src/main/java/ru/tuxoft/BookShopApplication.java
@@ -0,0 +1,12 @@
+package ru.tuxoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShopApplication.class, args);
+	}
+}
---
+++ src/main/java/ru/tuxoft/book/BookController.java
@@ -0,0 +1,10 @@
+package ru.tuxoft.book;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+}
