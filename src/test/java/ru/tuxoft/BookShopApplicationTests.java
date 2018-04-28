98e2fb8b96d4897337ab2a93142da12a8ba614d2
V.Darmakheev <v.darmakheev@playa.ru>
2018-04-28 14:17:59 +0800

    поправил тестовый конфиг

--- src/test/java/ru/tuxoft/BookShopApplicationTests.java
+++ src/test/java/ru/tuxoft/BookShopApplicationTests.java
@@ -3,10 +3,10 @@ package ru.tuxoft;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@Profile("test")
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookShopApplicationTests {
