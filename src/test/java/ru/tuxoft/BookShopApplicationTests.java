f81c6e27f91b671c79ca4b41567f3440d7314a97
V.Darmakheev <v.darmakheev@playa.ru>
2018-04-28 14:06:33 +0800

    добавил тестовый конфиг

--- src/test/java/ru/tuxoft/BookShopApplicationTests.java
+++ src/test/java/ru/tuxoft/BookShopApplicationTests.java
@@ -3,8 +3,10 @@ package ru.tuxoft;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Profile("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookShopApplicationTests {
