230357eafc6457eb577f9ca3dac06c048f05f28a
Artem Vershinin <art.vershinin@playa.ru>
2018-05-24 18:04:56 +0800

    #17 Прикрутить авторизацию

---
+++ src/main/java/ru/tuxoft/secure/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package ru.tuxoft.secure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry conf) {
+        conf.antMatchers("/categoryCarousels").authenticated();
+        conf.antMatchers("/**").permitAll();
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry conf = http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .authorizeRequests();
+        configure(conf);
+    }
+
+}
